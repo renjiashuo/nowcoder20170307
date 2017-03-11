39b05b33e508d26497c7a3c581072b22862c4601
renjiashuo <renjiashuo1993@qq.com>
2017-03-11 23:06:18 +0800

    nowcoder

--- nowcoder20170307/1.cpp
+++ nowcoder20170307/1.cpp
@@ -1,47 +1,59 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////
+//链接：https://www.nowcoder.com/questionTerminal/fb511c3f1ac447309368d7e5432c6c79
+//来源：牛客网
+//
+//如果一个数字能表示为p^q(^表示幂运算)且p为一个素数, q为大于1的正整数就称这个数叫做超级素数幂。现在给出一个正整数n, 如果n是一个超级素数幂需要找出对应的p, q。
+//
+//输入描述 :
+//输入一个正整数n(2 ≤ n ≤ 10 ^ 18)
+//
+//
+//输出描述 :
+//	 如果n是一个超级素数幂则输出p, q, 以空格分隔, 行末无空格。
+//	 如果n不是超级素数幂，则输出No
+//
+//
+//输入例子:
+//27
+//
+//
+//输出例子 :
+//	 3 3
+//////////////////////////////////////////////////////////////////////////
+
+bool primeNumber(long long p)
+{
+	for (int i = 2; i <= sqrt(p); i++)
+	{
+		if (p % i == 0)
+			return false;
+	}
+	return true;
+}
+
 int main()
 {
-	long long n = 0;
+	long long n = 0, p = 0, q = 0;
 	cin >> n;
 	if (n < 2)
 	{
 		cout << "No";
 		return 0;
 	}
-	long long p = 0, q = 0;
-	bool mark = false;
-	for (p = 2; p < n / 2; p++)
+	for (q = 2; q < n; q++)
 	{
-		if (n % p == 0)
+		p = pow(n, 1.0 / q);
+		if (pow(p, q) == n && primeNumber(p))
 		{
-			if (mark == false)
-				mark = true;
-			else
-			{
-				cout << "No";
-				return 0;
-			}
-			q = 1;
-			while (n != p)
-			{
-				if (n % p != 0)
-				{
-					cout << "No";
-					return 0;
-				}
-				else
-				{
-					n /= p;
-					q++;
-				}
-			}
-			break;
+			cout << p << " " << q;
+			return 0;
 		}
 	}
-	cout << p << " " << q;
-
+	cout << "No";
 	return 0;
 }
--- nowcoder20170307/3.cpp
+++ nowcoder20170307/3.cpp
@@ -4,6 +4,28 @@
 
 using namespace std;
 
+//////////////////////////////////////////////////////////////////////////
+//链接：https://www.nowcoder.com/questionTerminal/3a003cb6a3174ef9835fa603e01d8b52
+//来源：牛客网
+//
+//牛牛新买了一本算法书，算法书一共有n页，页码从1到n。牛牛于是想了一个算法题目：在这本算法书页码中0~9每个数字分别出现了多少次？
+//
+//输入描述 :
+//输入包括一个整数n(1 ≤ n ≤ 1, 000, 000, 000)
+//
+//
+//输出描述 :
+//	 输出包括一行10个整数，即0~9这些数字在页码中出现的次数，以空格分隔。行末无空格。
+//
+//
+//输入例子:
+//999
+//
+//
+//输出例子 :
+//	 189 300 300 300 300 300 300 300 300 300
+//////////////////////////////////////////////////////////////////////////
+
 int NumberOf0Between1AndN_Solution(int n)
 {
 	if (n == 0)
@@ -16,7 +38,7 @@ int NumberOf0Between1AndN_Solution(int n)
 		temp_n = temp_n / 10;
 		length++;
 	}
-	for (int i = 0; i < length - 1; i++)
+	for (int i = 0; i < length; i++)
 	{
 		int front = n / pow(10, i + 1);
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
@@ -24,12 +46,16 @@ int NumberOf0Between1AndN_Solution(int n)
 		int backLength = i;
 		if (mid == 0)
 		{
-			result += pow(10, backLength) * front;
+			result += pow(10, backLength) * front + back + 1;
 		}
 		else
 		{
 			result += pow(10, backLength) * (front + 1);
 		}
+	}
+	for (int i = 0; i < length; i++)
+	{
+		result -= pow(10, i);
 	}
 
 	return result;
@@ -53,7 +79,7 @@ int NumberOf1Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 1)
 		{
 			result += pow(10, backLength) * front;
 		}
@@ -88,11 +114,11 @@ int NumberOf2Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 2)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 2)
+		else if (mid == 2)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -123,11 +149,11 @@ int NumberOf3Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 3)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 3)
+		else if (mid == 3)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -158,11 +184,11 @@ int NumberOf4Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 4)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 4)
+		else if (mid == 4)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -193,11 +219,11 @@ int NumberOf5Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 5)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 5)
+		else if (mid == 5)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -228,11 +254,11 @@ int NumberOf6Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 6)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 6)
+		else if (mid == 6)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -263,11 +289,11 @@ int NumberOf7Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 7)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 7)
+		else if (mid == 7)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -298,11 +324,11 @@ int NumberOf8Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 8)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 8)
+		else if (mid == 8)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -333,11 +359,11 @@ int NumberOf9Between1AndN_Solution(int n)
 		int mid = (n - front * pow(10, i + 1)) / pow(10, i);
 		int back = n % (int)pow(10, i);
 		int backLength = i;
-		if (mid == 0)
+		if (mid < 9)
 		{
 			result += pow(10, backLength) * front;
 		}
-		else if (mid <= 9)
+		else if (mid == 9)
 		{
 			result += pow(10, backLength) * front + back + 1;
 		}
@@ -350,75 +376,73 @@ int NumberOf9Between1AndN_Solution(int n)
 	return result;
 }
 
-void Number(int n)
-{
-	int num[10] = { 0 };
-	for (int i = 1; i <= n; i++)
-	{
-		int N = i;
-		while (N != 0)
-		{
-			int N_last = N % 10;
-			switch (N_last)
-			{
-			case 0:
-				num[0]++;
-				break;
-			case 1:
-				num[1]++;
-				break;
-			case 2:
-				num[2]++;
-				break;
-			case 3:
-				num[3]++;
-				break;
-			case 4:
-				num[4]++;
-				break;
-			case 5:
-				num[5]++;
-				break;
-			case 6:
-				num[6]++;
-				break;
-			case 7:
-				num[7]++;
-				break;
-			case 8:
-				num[8]++;
-				break;
-			case 9:
-				num[9]++;
-				break;
-			}
-			N /= 10;
-		}
-	}
-	for (int i = 0; i < 9; i++)
-	{
-		cout << num[i] << " ";
-	}
-	cout << num[9];
-}
+//void Number(int n)
+//{
+//	int num[10] = { 0 };
+//	for (int i = 1; i <= n; i++)
+//	{
+//		int N = i;
+//		while (N != 0)
+//		{
+//			int N_last = N % 10;
+//			switch (N_last)
+//			{
+//			case 0:
+//				num[0]++;
+//				break;
+//			case 1:
+//				num[1]++;
+//				break;
+//			case 2:
+//				num[2]++;
+//				break;
+//			case 3:
+//				num[3]++;
+//				break;
+//			case 4:
+//				num[4]++;
+//				break;
+//			case 5:
+//				num[5]++;
+//				break;
+//			case 6:
+//				num[6]++;
+//				break;
+//			case 7:
+//				num[7]++;
+//				break;
+//			case 8:
+//				num[8]++;
+//				break;
+//			case 9:
+//				num[9]++;
+//				break;
+//			}
+//			N /= 10;
+//		}
+//	}
+//	for (int i = 0; i < 9; i++)
+//	{
+//		cout << num[i] << " ";
+//	}
+//	cout << num[9];
+//}
 
 int main()
 {
 	int n = 999;
 	cin >> n;
 
-	Number(n);
-
-	//cout << NumberOf0Between1AndN_Solution(n) << " ";
-	//cout << NumberOf1Between1AndN_Solution(n) << " ";
-	//cout << NumberOf2Between1AndN_Solution(n) << " ";
-	//cout << NumberOf3Between1AndN_Solution(n) << " ";
-	//cout << NumberOf4Between1AndN_Solution(n) << " ";
-	//cout << NumberOf5Between1AndN_Solution(n) << " ";
-	//cout << NumberOf6Between1AndN_Solution(n) << " ";
-	//cout << NumberOf7Between1AndN_Solution(n) << " ";
-	//cout << NumberOf8Between1AndN_Solution(n) << " ";
-	//cout << NumberOf9Between1AndN_Solution(n);
+	cout << NumberOf0Between1AndN_Solution(n) << " ";
+	cout << NumberOf1Between1AndN_Solution(n) << " ";
+	cout << NumberOf2Between1AndN_Solution(n) << " ";
+	cout << NumberOf3Between1AndN_Solution(n) << " ";
+	cout << NumberOf4Between1AndN_Solution(n) << " ";
+	cout << NumberOf5Between1AndN_Solution(n) << " ";
+	cout << NumberOf6Between1AndN_Solution(n) << " ";
+	cout << NumberOf7Between1AndN_Solution(n) << " ";
+	cout << NumberOf8Between1AndN_Solution(n) << " ";
+	cout << NumberOf9Between1AndN_Solution(n);
 
 	return 0;
 }
